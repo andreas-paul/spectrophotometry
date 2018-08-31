bb7811c08f1a29ea33a1c8930dc01fb19bab7f57
Andreas Paul <35452653+andreas-paul@users.noreply.github.com>
2018-08-31 17:11:59 +0200

    updated code

--- py_photospec_conv_to_rgb.py
+++ py_photospec_conv_to_rgb.py
@@ -1,63 +1,63 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jan 10 12:53:51 2018
-Updated on Mon Aug 06 2018
+Updated 06 Aug 2018
+
 @author: A Paul
-Python version: 3.6
 """
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import matplotlib as mpl
+# import matplotlib.image as mpimg
 
-# Data import and extraction of columns as individual variables. The .csv file should have the following shape:
-#  Column 1: L* | Column 2: a* | Column 3: b*
-lab_data = pd.read_csv('photospectrometry.csv')
-var_y = (lab_data.iloc[:,0] + 16) / 116
-var_x = lab_data.iloc[:,1] / 500 + var_y
-var_z = var_y - lab_data.iloc[:,2] / 200
+"""
+Data import and extraction of columns as individual variables, variable definitions
+"""
+#lab_1100 = pd.read_csv('photospectrometry.csv', names=['depth_m', 'L*', 'a*', 'b*'], header=None)
+lab_1100 = pd.read_csv('photospectrometry_filled.csv', names=['depth_m', 'L*', 'a*', 'b*'], header=None)
+depth_m = lab_1100['depth_m']
+L = lab_1100['L*'] # L* value for calculations
+a = lab_1100['a*'] # a* value for calculations
+b = lab_1100['b*'] # b* value for calculations
+
+E = 0.008856 # epsilon
+K = 903.3
 
 
 """
-Conversion from LAB to XYZ (It is not possible to convert directly from LAB to sRGB)
+Conversion from LAB to XYZ
 """
-# The variables are cubed. Normally the cubed values would be checked if they are larger than 0.008856. If they are, then the respective value is cubed. If not, the following formula should be used to calculate the respective value: var = (var - 16 / 116) / 7.787
-var_y = np.power(var_y,3)
-var_x = np.power(var_x,3)
-var_z = np.power(var_z,3)
+fy = (L + 16) / 116
+fx = a / 500 + fy
+fz = fy - b / 200
 
-# The calculated variables  are now multiplied with reference values based on observer angle of 10 degrees and the illuminant D65
-X = var_x * 94.811
-Y = var_y * 100.000
-Z = var_z * 107.304
+var_x = np.where(np.power(fx, 3) > E, np.power(fx, 3), (116 * fx - 16) / K)
+var_y = np.where(L > K * E, np.power((L + 16) / 116,3), L / K )
+var_z = np.where(np.power(fz,3) > E, np.power(fz,3), (116 * fz - 16) / K)
 
-X_fr = pd.DataFrame(X, columns=['X'])
-Y_fr = pd.DataFrame(Y, columns=['L*'])
-Z_fr = pd.DataFrame(Z, columns=['Z'])
+# Chromatic Adaptation
+var_x = var_x * 0.95047
+var_y = var_y * 1.000
+var_z = var_z * 1.08883
 
-Y_fr.columns = ['Y']
 
 """
 Conversion from XYZ to sRGB
 """
-# Taken from EasyRGB
-var_X = X_fr / 100
-var_Y = Y_fr / 100
-var_Z = Z_fr / 100
 
-var_R = (var_X.iloc[:,0] * 3.2404542) + (var_Y.iloc[:,0] * -1.5371385) +  (var_Z.iloc[:,0] * -0.4985314)
-var_G = (var_X.iloc[:,0] * -0.9692660) + (var_Y.iloc[:,0] * 1.8760108) + (var_Z.iloc[:,0] * 0.0415560)
-var_B = (var_X.iloc[:,0] * 0.0556434) + (var_Y.iloc[:,0] * -0.2040259) + (var_Z.iloc[:,0] * 1.0572252)
+# Taken from EasyRGB, http://www.brucelindbloom.com/
+var_R = (var_x * 3.2404542) + (var_y * -1.5371385) + (var_z * -0.4985314)
+var_G = (var_x * -0.9692660) + (var_y * 1.8760108) + (var_z * 0.0415560)
+var_B = (var_x * 0.0556434) + (var_y * -0.2040259) + (var_z * 1.0572252)
 
-print(min(var_R))
-print(min(var_G))
-print(min(var_B))
-
+# sRGB Companding
 power = 1/2.4
-var_R = np.power((var_R * 1.055),power) - 0.055
-var_G = np.power((var_G * 1.055), power) - 0.055
-var_B = np.power((var_B * 1.055), power) - 0.055
+var_R = np.where(var_R <= 0.0031308, var_R * 12.92, 1.055 * np.power(var_R,power) - 0.055  )
+var_G = np.where(var_G <= 0.0031308, var_G * 12.92, 1.055 * np.power(var_G,power) - 0.055  )
+var_B = np.where(var_B <= 0.0031308, var_B * 12.92, 1.055 * np.power(var_B,power) - 0.055  )
+
 
 sR = var_R * 255
 sG = var_G * 255
@@ -66,20 +66,25 @@ sB = var_B * 255
 zipped = zip(var_R,var_G,var_B)
 sRGB_list = list(zipped)
 
+#zipped_255 = zip(sR, sG, sB)
+#sRGB_list_255 = list(zipped_255)
+#sRGB_list_255 = pd.DataFrame(sRGB_list_255)
+#sRGB_list_255['depth_m'] = depth_m
+#np.savetxt("sRGB_list_255.csv", sRGB_list_255, delimiter=",", fmt='%f')
 
-"""
-Plotting
-"""
 # Create custom colormap from sRGB list and plot the colormap
-fig = plt.figure(figsize=[20,2], facecolor='white')
+max_depth = np.amax(lab_1100.iloc[:,0])
+fig = plt.figure(figsize=[max_depth,2], facecolor='white')
 ax = fig.subplots()
 cmap = ListedColormap(sRGB_list)
 cmap.set_over('0.25')
 cmap.set_under('0.75')
 bounds = np.arange(0,cmap.N,1)
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-cb2 = mpl.colorbar.ColorbarBase(ax, cmap=cmap,                orientation='horizontal')
+cb2 = mpl.colorbar.ColorbarBase(ax, cmap=cmap,
+                    orientation='horizontal')
 cb2.set_label('')
+cb2.outline.set_visible(False)
+cb2.set_ticks([])
 ax.set_xticklabels('')
-plt.show()
-plt.savefig('colormap_data.png',dpi=1200)
+plt.savefig('colormap_1100.png',dpi=1200)
