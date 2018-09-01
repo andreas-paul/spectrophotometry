1dd305a746e13b1d0f7215635f3a0083722cc767
Andreas Paul <35452653+andreas-paul@users.noreply.github.com>
2018-09-01 22:16:44 +0200

    edited readme and code

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 Colour is an important parameter in the geosciences. It is used to describe sediment and often allows for the interpretation of depositional environment, estimate organic content or aids in the identification of geological boundaries, amongst others. 
 
-In core analysis, colour is measured either manually or automatically with spectrophotometers. The output of these spectrophotometers, at least when we conducted these measurements more than 10 years ago, was a dataset in CIE-LAB format. While it would, theoretically, be possible to plot this data, it is much more straight forward to simply convert the data to the sRGB colorspace. 
+In core analysis, colour is measured either manually or automatically with spectrophotometers. The output of these spectrophotometers, at least when we conducted these measurements more than 10 years ago, was a dataset in CIE-LAB format. While it is, theoretically, be possible to plot this data, it is much more straight forward to simply convert the data to the sRGB colorspace which can easily be used in many different programs.
 
-An example is shown below, where on the left the raw CIE-LAB data is shown, and on the right the final colormap of the sediment.
+An example of the results is shown below. On the left the raw CIE-LAB data, on the right the final colormap calculated from the CIE-LAB data.
 
 <p align="center">
 <img src="example.png" width="500">
 </p>
 
+There is very little information available about color conversion from official sources. Hence, most of the calculations used in the code were taken from information provided by www.easyrgb.com and http://www.brucelindbloom.com/. 
--- py_photospec_conv_to_rgb.py
+++ py_photospec_conv_to_rgb.py
@@ -5,8 +5,6 @@ Updated 06 Aug 2018
 Updated 31 Aug 2018
 
 @author: A Paul
-
-This code will convert CIE Lab data to XYZ and sRGB values. Information in the web is sparse, so most of the calculations were taken from https://www.easyrgb.com and http://www.brucelindbloom.com/. 
 """
 import pandas as pd
 import numpy as np
@@ -15,9 +13,9 @@ from matplotlib.colors import ListedColormap
 import matplotlib as mpl
 
 
-"""
-Data import and extraction of columns as individual variables, variable definitions
-"""
+
+# Data import and extraction of columns as individual variables, variable definitions
+
 lab_core = pd.read_csv('photospectrometry.csv', names=['depth_m', 'L*', 'a*', 'b*'], header=None)
 depth_m = lab_core['depth_m']
 L = lab_core['L*'] # L* value for calculations
@@ -28,9 +26,9 @@ E = 0.008856 # epsilon
 K = 903.3
 
 
-"""
-Conversion from LAB to XYZ
-"""
+
+# Conversion from LAB to XYZ
+
 fy = (L + 16) / 116
 fx = a / 500 + fy
 fz = fy - b / 200
@@ -45,9 +43,9 @@ var_y = var_y * 1.000
 var_z = var_z * 1.08883
 
 
-"""
-Conversion from XYZ to sRGB
-"""
+
+# Conversion from XYZ to sRGB
+
 
 var_R = (var_x * 3.2404542) + (var_y * -1.5371385) + (var_z * -0.4985314)
 var_G = (var_x * -0.9692660) + (var_y * 1.8760108) + (var_z * 0.0415560)
@@ -63,16 +61,12 @@ sR = var_R * 255
 sG = var_G * 255
 sB = var_B * 255
 
+
+# Create custom colormap from sRGB list and plot the colormap. This is the easiest way to create a visualisation that looks like a drilling core. Take note that the figure does not take into account any gaps in a record and will produce a continous record. I suggest to fill all gaps either by white (CIE Lab: 100 0 0) or interpolate the gaps with the values above and below. 
+
 zipped = zip(var_R,var_G,var_B)
 sRGB_list = list(zipped)
 
-"""
-Create custom colormap from sRGB list and plot the colormap. This is the easiest way to create a visualisation that looks like a drilling core. Take note that the figure does not take into account any gaps in a record and will produce a continous record. I suggest to fill all gaps either by white (CIE Lab: 100 0 0) or interpolate the gaps with the values above and below. 
-"""
-
-
-# max_depth = np.amax(lab_core.iloc[:,0])
-# fig = plt.figure(figsize=[max_depth,2], facecolor='white')
 fig = plt.figure()
 ax = fig.subplots()
 cmap = ListedColormap(sRGB_list)
